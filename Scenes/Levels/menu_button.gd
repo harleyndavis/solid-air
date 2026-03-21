513a4573f4e2c182ed645b506c4a40ceab221a7f
Harley Davis <asphixiatedaphid@gmail.com>
2026-03-21 00:24:28 -0700

    Finally changed from gear symbol to hamburger symbol as the gear wouldnt' load in HTML5

--- Scenes/Levels/menu_button.gd
+++ Scenes/Levels/menu_button.gd
@@ -4,6 +4,7 @@ signal menu_pressed
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 		self.pressed.connect(_on_button_pressed)
+		
 
 func _on_button_pressed() -> void:
 	emit_signal("menu_pressed")
